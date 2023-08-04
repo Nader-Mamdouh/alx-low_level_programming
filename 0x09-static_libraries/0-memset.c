34d5b493cd60e56ff01823d2d2b18e0485b2d658
Nader Mamdouh <nadermamdouh20@gmail.com>
2023-08-04 13:57:22 -0700

    updated

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,10 +1,33 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
+/**
+ * *_memset - function declaration
+ * @s: pointer to constant
+ * @b: constant
+ * @n: number of bytes
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * *_memset - function definition
+ * @s: pointer to a constant
+ * @b: constant to be used
+ * @n: maximum number of bytes
+ * Description: fills a memory address with constant byte
+ * Return: pointer s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    return memset(s, b, n);
+	unsigned int index;
+
+	for (index = 0; n > 0; index++)
+	{
+		s[index] = b;
+		n--;
+	}
+
+	return (s);
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,9 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include "2-strlen.c"
+
+/**
+ * _strcpy - function declaration
+ * @dest: destination to copy
+ * @src: source to copy
+ * Return: destination
+ */
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strcpy - function declaration
+ * Description: copies a string
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * Return: destination
+ */
+
 char *_strcpy(char *dest, char *src)
 {
-    return strcpy(dest, src);
+	int index;
+
+	for (index = 0; index <= _strlen(src); index++)
+	{
+		dest[index] = src[index];
+	}
+	return (dest);
 }
